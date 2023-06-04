582a4ebe4500602fc75f092136147b1512e135a0
RudiJr <rudi.juniior@gmail.com>
2023-06-04 16:59:16 -0300

    Created main.js file and geting the books from the API

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let livros = [];
+const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+getLivrosAPI();
+
+async function getLivrosAPI(){
+    const res = await fetch(endpointAPI);
+    livros = await res.json();
+    console.table(livros);
+};
