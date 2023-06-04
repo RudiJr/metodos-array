ffc6dcadc0ff1f09ff1e6c3850f615162ed18571
RudiJr <rudi.juniior@gmail.com>
2023-06-04 17:46:00 -0300

    Geting all the books and putting them on screen

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 let livros = [];
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getLivrosAPI();
+const insertLivros = document.getElementById('livros');
 
 async function getLivrosAPI(){
     const res = await fetch(endpointAPI);
     livros = await res.json();
+
     console.table(livros);
+    exibirLivros(livros);
 };
+
+function exibirLivros(listaLivros){
+    listaLivros.forEach(livro  => {
+        insertLivros.innerHTML += `<div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">${livro.titulo}</h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$ ${livro.preco}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+      </div>`
+    });
+}
