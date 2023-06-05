5682145c69fc566f8d00144da6c92f35dad55e03
RudiJr <rudi.juniior@gmail.com>
2023-06-05 12:08:07 -0300

    Created a filter to see if the book is available

--- js/filter.js
+++ js/filter.js
@@ -4,6 +4,6 @@ buttons.forEach(btn => btn.addEventListener('click', filtrarLivros));
 function filtrarLivros(){
     const btnCategoria = document.getElementById(this.id);
     const categoria = btnCategoria.value;
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) :livros.filter(livro => livro.categoria == categoria);
    exibirLivros(livrosFiltrados);
 };
