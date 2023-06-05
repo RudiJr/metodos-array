9e8a110a20f8b3f438f3c4703cbb5a793290c9cb
RudiJr <rudi.juniior@gmail.com>
2023-06-05 12:28:42 -0300

    Improving the readability of the code

--- js/filter.js
+++ js/filter.js
@@ -4,6 +4,23 @@ buttons.forEach(btn => btn.addEventListener('click', filtrarLivros));
 function filtrarLivros(){
     const btnCategoria = document.getElementById(this.id);
     const categoria = btnCategoria.value;
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) :livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarDisponibilidade() : filtrarCategoria(categoria);
    exibirLivros(livrosFiltrados);
+   if(categoria == 'disponivel'){
+    exibirValorTotal();
+   };
+};
+
+function filtrarCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotal(){
+    valorTotal.innerHTML = `<div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+  </div>`;
 };
