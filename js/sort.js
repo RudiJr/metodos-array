e9e3994050f7310959446ed43d9ae3eb5c5fc801
RudiJr <rudi.juniior@gmail.com>
2023-06-05 11:25:24 -0300

    Created sort.js file and created and function to order by price

---
+++ js/sort.js
@@ -0,0 +1,8 @@
+let btnOrdenarPreco = document.getElementById('btnOrdenarPorPreco');
+btnOrdenarPreco = addEventListener('click', ordenarPorPreco);
+
+function ordenarPorPreco(){
+    let livrosOrdenados = livros.sort((a,b) => a.preco - b.preco);
+    exibirLivros(livrosOrdenados);
+};
+
